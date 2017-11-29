f649b7e8c71f8e341b379412f2857c35d7045c19
levitan <levitan@msn.cn>
2017-11-30 00:12:52 +0800

    v2.0: ADD auto running and email send functions

--- web.py
+++ web.py
@@ -8,6 +8,7 @@ import imageResize
 from PIL import Image,ImageDraw,ImageFont
 import json
 from selenium import webdriver
+import sendEmail
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -42,12 +43,12 @@ def wenzi(im,state):
     text4 = u'天气预报'
     draw.text((20, 1820), text4, font=font, fill='#000000')
 
-    return  im
+    return  im,text
 
 def Yuntu(arr):
     global dayNow
     global state
-    toImage = Image.new('RGBA', (900, 2200))
+    toImage = Image.new('RGBA', (900, 2200),'#FFFFFF')
     for i in range(4):
         fromImge = Image.open(arr[i])
         loc = (50,(i * 600+270))
@@ -55,10 +56,10 @@ def Yuntu(arr):
             loc = (50,1870)
         print(loc)
         toImage.paste(fromImge, loc)
-
-    toImage=wenzi(toImage,state)
-
-    toImage.save("./"+dayNow+'/weatheReport.png')
+    toImage,info_text=wenzi(toImage,state)
+    img_file="./"+dayNow+'/%s weatheReport.png'%dayNow
+    toImage.save(img_file)
+    return info_text,img_file
 
 def mkdir(path):
     # 引入模块
@@ -126,17 +127,17 @@ def get_all_and_get_midnightUrl(imglist):
     return midnightUrl
 
 
-def getImg(html):
+def getImg(html,weather_clock_url,weather_forecast_url):
     midnight=0
     d, t = GetNowTime()
     mkdir(d)
     ########晴天钟图################
-    weather_clock_url = "http://202.127.24.18/v4/bin/astro.php?lon=87.177&lat=43.475&lang=zh-CN&ac=2000&unit=metric&tzshift=0"
+    # weather_clock_url = "http://202.127.24.18/v4/bin/astro.php?lon=87.177&lat=43.475&lang=zh-CN&ac=2000&unit=metric&tzshift=0"
     weather_clock="./" + str(d)+"/"+str(t) + "_weather_clock.jpg"
     download(weather_clock_url, weather_clock)
 
     ########天气预报图################
-    weather_forecast_url = "http://202.127.24.18/v4/bin/civillight.php?lon=87.177&lat=43.475&lang=zh-CN&ac=2000&unit=metric&output=internal&tzshift=0"
+    # weather_forecast_url = "http://202.127.24.18/v4/bin/civillight.php?lon=87.177&lat=43.475&lang=zh-CN&ac=2000&unit=metric&output=internal&tzshift=0"
     weather_forecast="./" + str(d) + "/" + str(t) + "_weather_ forecast.jpg"
     download(weather_forecast_url, weather_forecast)
     imgID = 'Image_asc'
@@ -146,7 +147,7 @@ def getImg(html):
     imgMorning='%s_%s_%s.jpg'%(imglist[0][-7:-6],imglist[0][-5:-3],imglist[0][-2:])
     savePath1='%s\\%s'%(d,imgMorning)
     print 'image morning: %s'%savePath1
-    midnightHour=1
+    midnightHour=2
     nowHour=int(imglist[0][-7])
     if nowHour<midnightHour :
         print 'Please run this program at time later than %s hour '%midnightHour
@@ -170,8 +171,9 @@ def getImg(html):
     # urls=[midnightUrl,nowUrl,weather_clock_url,weather_forecast_url]
     urls = [ weather_clock_url, weather_forecast_url]
 
-    weatherReport(savePath1,savePath2,urls)
+    arr_final=weatherReport(savePath1,savePath2,urls)
     print savePath1,savePath2,urls
+    return arr_final
 
 def weatherReport(imgMorn,imgMid,urls):
     global state
@@ -192,13 +194,18 @@ def weatherReport(imgMorn,imgMid,urls):
             a.fixed_size(768, 184)
         arr.append("./"+dayNow+'/'+str(i)+'_1.jpg')
         #arr=
-    Yuntu(arr)
+    return arr
 
-def get_state(version=1):
+def report(arr_final):
+    msg_text,img_file=Yuntu(arr_final)
+    return msg_text,img_file
+
+
+def get_state(version,ApiUrl):
     global state
 
     ########晴天钟json数据################
-    ApiUrl = "http://202.127.24.18/v4/bin/astro.php?lon=87.177&lat=43.475&ac=0&lang=en&unit=metric&output=json&tzshift=0"
+    # ApiUrl = "http://202.127.24.18/v4/bin/astro.php?lon=87.177&lat=43.475&ac=0&lang=en&unit=metric&output=json&tzshift=0"
 
     html = urllib.urlopen(ApiUrl)
     data = html.read().decode("utf-8")
@@ -228,18 +235,23 @@ def get_state(version=1):
                 state[i + 1] = u"良"
             if cloudcover_state[i + 1] > 7:
                 state[i + 1] = u"差"
+        print (state)
+        return state
 
     ########前一天晚上运行，得到凌晨状况，默认优################
     if version == 2:
+
         cloudcover_yesterday = (cloudcover[27] + cloudcover[30]) / 2
+        cloudcover_state[0]=cloudcover_yesterday
         if cloudcover_state[0] > 0:
             state[0] = u"优"
         if cloudcover_state[0] > 4:
             state[0] = u"良"
         if cloudcover_state[0] > 7:
             state[0] = u"差"
-        pass
-    print (state)
+        print (state)
+        return state
+
 
 def webscreen(url,eleID,savepath1,savepath2):
     driver = webdriver.PhantomJS()
@@ -282,18 +294,88 @@ def webscreen(url,eleID,savepath1,savepath2):
             continue
     return im1,im2
 
-if __name__ == '__main__' :
-
+def auto_weather_report_Nanshan(running_day,midnight_time,morning_time,check_min,url,email_info):
     global state
     global dayNow
-    dayNow = time.strftime(u"%Y_%m_%d", time.localtime(time.time()))
-    state = [u'优', u'优', u'优', u'优']
-    get_state()
+    # start_run=False
+    # while not start_run:
+    #     time_hour = int(time.strftime(u"%H", time.localtime(time.time())))
+    #     if time_hour!=0:
+    #         print 'now the time is %s hour, and have to wait to be %s hour to running program' % (
+    #         time_hour, 0)
+    #         print 'wait %s minutes' % check_min
+    #         time.sleep(check_min * 60)
+    #     else:
+    #         start_run=True
+    #         print 'the auto weather report program is start to running...'
 
-    ########天文台全天相机################
-    html = getHtml("http://119.78.162.41:81/asc_main.aspx")
-    # html = getHtml("http://119.78.162.41:81")
-    getImg(html)
+    for day in range(1,running_day+1):
+        print 'the auto weather report program for Nanshan is running in day %s, it will running for %s days'%(day,running_day)
+        day_now=time.strftime(u"%Y_%m_%d", time.localtime(time.time()))
+        dayNow=day_now
+        state=[u'NA', u'NA', u'NA', u'NA']
+        is_midnight=False
+        is_morning=False
+        while not is_midnight:
+            time_hour=int(time.strftime(u"%H", time.localtime(time.time())))
+            if time_hour!=0:
+                print 'now the time is %s hour, and have to wait to be %s hour to check midnight state'%(time_hour,0)
+                print 'wait %s minutes'%check_min
+                time.sleep(check_min*60)
+            else:
+                print 'now time is %s in day %s midnight, check weather state...'%(time_hour,day_now)
+                is_midnight=True
+        get_state(2,url[3])
+        while not is_morning:
+            time_hour = int(time.strftime(u"%H", time.localtime(time.time())))
+            if time_hour < morning_time:
+                print 'now the time is %s hour, and have to wait to be %s hour to check morning state' % (
+                time_hour, morning_time)
+                print 'wait %s minutes' % check_min
+                time.sleep(check_min * 60)
+            else:
+                print 'now time is %s hour in day %s morning , check weather state...'%(time_hour,day_now)
+                is_morning = True
+        get_state(1,url[3])
+        html = getHtml(url[0])
+        arr_final = getImg(html,url[1],url[2])
+        msg_text, img_file = report(arr_final)
+        sendEmail.send_email(email_info, msg_text, img_file, day_now)
+        print '%s weather report have send by email !'%day_now
+
+def auto_weather_report_Nanshan_test():
+    [running_day, midnight_time, morning_time, check_min]=[30,2,9,5]
+    camera_url="http://119.78.162.41:81/asc_main.aspx"
+    email_info = ['daihui@mail.ustc.edu.cn', 'XXX', 'levitan@msn.cn', 'mail.ustc.edu.cn']
+    weather_clock_url = "http://202.127.24.18/v4/bin/astro.php?lon=87.177&lat=43.475&lang=zh-CN&ac=2000&unit=metric&tzshift=0"
+    weather_forecast_url = "http://202.127.24.18/v4/bin/civillight.php?lon=87.177&lat=43.475&lang=zh-CN&ac=2000&unit=metric&output=internal&tzshift=0"
+    ApiUrl = "http://202.127.24.18/v4/bin/astro.php?lon=87.177&lat=43.475&ac=0&lang=en&unit=metric&output=json&tzshift=0"
+    url=[camera_url,weather_clock_url,weather_forecast_url,ApiUrl]
+
+    print 'you set running information : running days: %s, midnight hour: %s, morning hour: %s, check minutes: %s'\
+          %(running_day,midnight_time,morning_time,check_min)
+    print 'your cloud camera url: %s'%camera_url
+    print 'weather report will be send to email: %s'%email_info[2]
+
+    auto_weather_report_Nanshan(running_day, midnight_time, morning_time, check_min,url,email_info)
+
+if __name__ == '__main__' :
+
+    # global state
+    # global dayNow
+    # dayNow = time.strftime(u"%Y_%m_%d", time.localtime(time.time()))
+    # state = [u'差', u'优', u'优', u'优']
+    # get_state(1,)
+    #
+    # ########天文台全天相机################
+    # html = getHtml("http://119.78.162.41:81/asc_main.aspx")
+    # # html = getHtml("http://119.78.162.41:81")
+    # arr_final=getImg(html)
+    # msg_text, img_file=report(arr_final)
+    # info=['daihui@mail.ustc.edu.cn','levitan2012','levitan@msn.cn','mail.ustc.edu.cn']
+    # sendEmail.send_email(info,msg_text,img_file,dayNow)
+    auto_weather_report_Nanshan_test()
+
 
 
 
